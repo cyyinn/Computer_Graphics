ffbbfef3255c174daf8930729527ec75900807b6
cyyinn <107969965+cyyinn@users.noreply.github.com>
2022-07-05 22:33:56 +0800

    Update 未命名16.cpp

--- 图形学 陈怡楠 20201900299/实验(14)/未命名16.cpp
+++ 图形学 陈怡楠 20201900299/实验(14)/未命名16.cpp
@@ -1,91 +1,96 @@
-#include <stdlib.h>
-#include <GL/glut.h>  
-float theta=0.0;
-void drawPyramid() //该金字塔在以原点为中心，边长为2的立方体范围内
+#include<gl/glut.h>
+#include<stdio.h>
+#include<math.h>
+
+GLsizei winWidth = 600, winHeight = 600;
+GLfloat xwcMin = -50.0, xwcMax = 50.0;
+GLfloat ywcMin = -50.0, ywcMax = 50.0;
+
+class wcPt3D {
+public:
+	GLfloat x, y, z;
+};
+
+void init(void)
 {
-	glBegin(GL_TRIANGLES);								
-	  glColor3f(1.0f,0.0f,0.0f);	  //前面为红色		
-	  glVertex3f( 0.0f, 1.0f, 0.0f);	//前面三角形上顶点
-	  glVertex3f(-1.0f,-1.0f, 1.0f);  //前面三角形左顶点
-	  glVertex3f( 1.0f,-1.0f, 1.0f);	//前面三角形右顶点	
-	  
-	  glColor3f(0.0f,1.0f,0.0f);		//右面为绿色				
-	  glVertex3f( 0.0f, 1.0f, 0.0f);	//右面三角形上顶点			
-	  glVertex3f( 1.0f,-1.0f, 1.0f);	//右面三角形左顶点				
-	  glVertex3f( 1.0f,-1.0f, -1.0f);	 //右面三角形右顶点	
-	  
-	  glColor3f(0.0f,0.0f,1.0f);		//背面为蓝色			
-	  glVertex3f( 0.0f, 1.0f, 0.0f);  //背面三角形上顶点				
-	  glVertex3f( 1.0f,-1.0f, -1.0f);	 //背面三角形左顶点
-	  glVertex3f(-1.0f,-1.0f, -1.0f);	//背面三角形右顶点	
- 	  
-	  glColor3f(1.0f,1.0f,0.0f);		//左面为黄色			
-	  glVertex3f( 0.0f, 1.0f, 0.0f);	//左面三角形上顶点
-	  glVertex3f(-1.0f,-1.0f,-1.0f);	//左面三角形左顶点
-	  glVertex3f(-1.0f,-1.0f, 1.0f);	//左面三角形右顶点	
-	glEnd();
-	glBegin(GL_POLYGON);  //金字塔底面正方形
-      glColor3f(0.5f,0.5f,0.5f);  //底面为灰色
-	  glVertex3f(-1.0f,-1.0f, 1.0f);  
-	  glVertex3f(1.0f,-1.0f, 1.0f);
-	  glVertex3f(1.0f,-1.0f, -1.0f);
-	  glVertex3f(-1.0f,-1.0f, -1.0f);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+}
+void poltPoint(wcPt3D bezCurvePt)
+{
+	glBegin(GL_POINTS);
+	glVertex2f(bezCurvePt.x, bezCurvePt.y);
 	glEnd();
 }
 
-void display()
+void binomialCoeffs(GLint n, GLint* C)
 {
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); //清空颜色和深度缓存
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	//gluLookAt(2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glTranslatef(0.0f,0.0f,-5.0f);
-	glRotatef(theta,0.0f,1.0f,0.0f);
-	drawPyramid();
-    
-	glutSwapBuffers();
-	
+	GLint k, j;
+	for (k = 0; k <= n; k++) {
+		C[k] = 1;
+		for (j = n; j >= k + 1; j--) 
+			C[k] *= j;
+		for (j = n - k; j >= 2; j--)
+			C[k] /= j;
+	}
 }
 
-void reshape(int w, int h) //重绘回调函数，在窗口首次创建或用户改变窗口尺寸时被调用
+void computeBezPt(GLfloat u, wcPt3D* bezPt, GLint nCtrlPts, wcPt3D* ctrlPts, GLint* C)
 {
-	glViewport(0, 0, w, h);
+	GLint k, n = nCtrlPts - 1;
+	GLfloat bezBlendFcn;
+	bezPt->x = bezPt->y = bezPt->z = 0.0;
+	for (k = 0; k < nCtrlPts; k++) {
+		bezBlendFcn = C[k] * pow(u, k) * pow(1 - u, n - k);
+		bezPt->x += ctrlPts[k].x * bezBlendFcn;
+		bezPt->y += ctrlPts[k].y * bezBlendFcn;
+		bezPt->z += ctrlPts[k].z * bezBlendFcn;
+	}
+}
+
+void bezier(wcPt3D* ctrlPts, GLint nCtrlPts, GLint nBezCurvePts)
+{
+	wcPt3D bezCurvePt;
+	GLfloat u;
+	GLint* C, k;
+	C = new GLint[nCtrlPts];
+	binomialCoeffs(nCtrlPts - 1, C);
+	for (k = 0; k <= nBezCurvePts; k++) {
+		u = GLfloat(k) / GLfloat(nBezCurvePts);
+		computeBezPt(u, &bezCurvePt, nCtrlPts, ctrlPts, C);
+		poltPoint(bezCurvePt);
+	}
+	delete[]C;
+}
+
+void displayFcn(void)
+{
+	GLint nCtrlPts = 4, nBezCurvePts = 1000;
+	wcPt3D ctrlPts[4] = { {-40.0,-40.0,0.0},{-10.0,200.0,0.0},{10.0,-200.0,0.0},{40.0,40.0,0.0} };
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(4);
+	glColor3f(1.0, 0.0, 0.0);
+	bezier(ctrlPts, nCtrlPts, nBezCurvePts);
+	glFlush();
+}
+
+void winReshapeFcn(GLint newWidth, GLint newwHeight)
+{
+	glViewport(0, 0, newwHeight, newwHeight);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	//glFrustum(-1.0, 1.0, -1.0, 1.0, 3.1, 10.0);
-	//gluPerspective(45,1,0.1,10.0);
-	glOrtho(-2.0, 2.0, -2.0, 2.0, 2.0, 10.0);
+	gluOrtho2D(xwcMin, xwcMax, ywcMin, ywcMax);
+	glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void init()
+void main(int argc, char** argv)
 {
-	glClearColor (1.0, 1.0, 1.0, 1.0); 
-	glEnable(GL_DEPTH_TEST);     //启动深度测试模式
-}
-
-void myKeyboard(unsigned char key,  int x, int y)
-{
-	if(key == 'a' || key == 'A')
-		theta += 5.0;
-	if(key == 's' || key == 'S')
-		theta -= 5.0;
-    if(key == 'c' || key == 'C')
-		exit(0);
-	if (theta>360) theta -=360;
-	if (theta<0) theta +=360;
-	glutPostRedisplay(); //重新调用绘制函数
-}
-
-int main(int argc, char** argv)
-{
-	glutInit(&argc,argv); 
-	glutInitDisplayMode (GLUT_DEPTH |GLUT_DOUBLE | GLUT_RGB);  
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(0,0); 
-	glutCreateWindow("金字塔---A键:顺时针旋转,S键:逆时针旋转,C键:退出"); 
-	glutReshapeFunc(reshape); //指定重绘回调函数
-	glutDisplayFunc(display);
-	glutKeyboardFunc( myKeyboard);   //指定键盘回调函数
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(50, 50);
+	glutInitWindowSize(winWidth, winHeight);
+	glutCreateWindow("Bezier Curve");
 	init();
+	glutDisplayFunc(displayFcn);
+	glutReshapeFunc(winReshapeFcn);
 	glutMainLoop();
 }
